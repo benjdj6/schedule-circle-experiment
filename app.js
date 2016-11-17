d4b4d60937ea76f2e83c7a5a69a20216c37b7e93
benjdj6 <benjdj6@vt.edu>
2016-11-16 19:31:42 -0500

    Time from form now converted into degrees for arc

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 
 var app = angular.module('schedule', []);
 
+function timeToDegrees(hour, minute, timeofday) {
+  if(timeofday === "pm" && hour != 12) {
+    hour += 12;
+  }
+  minute = minute/60.0;
+  ratio = (minute + hour)/24.0;
+  return ratio * 360.0;
+}
+
 app.factory('arcs', [function() {
   var o = {
     path: []
   };
 
   o.create = function(arc) {
-    console.log(arc);
-    return true;
+    var start = timeToDegrees(arc.sthour, arc.stmin, arc.sttod);
+    var end = timeToDegrees(arc.endhour, arc.endmin, arc.endtod);
+    console.log(start);
+    console.log(end);
   };
 
   return o;
